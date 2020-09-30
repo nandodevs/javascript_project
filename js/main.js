9d8e88ac10276894210603152f164b3b9bde3083
nando.devs <nando.devs@gmail.com>
2020-09-30 15:23:51 -0300

    Aprendendo JavaScript na DIO

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var frutas = [{nome:"maça", cor: "vermelha"}, {nome:"uva", cor: "roxa"}]//Dicionários
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/*var fruta = {nome:"maça", cor: "vermelha"}//Dicionários
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+
+/*var lista = ["maça", "pêra", "laranja"]; //Arrays
+lista.push("uva");
+//lista.pop();
+//PUSH adiciona elemento na lista e POP remove.
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));*/
+
+/*var nome = "Sisnando Junior";
+var idade = 33;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert( nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"));*/
