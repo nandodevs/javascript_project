4c57910c1ef7d7c0e01e5a1533c59c4983e2d86e
Sisnando Junior <nando.devs@gmail.com>
2020-09-30 22:35:34 -0300

    Projeto atualizado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,63 @@
-var frutas = [{nome:"maça", cor: "vermelha"}, {nome:"uva", cor: "roxa"}]//Dicionários
+function clicou(){
+    //alert("Obrigado por clicar!")
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar(){
+    //window.open("contato.html");
+    window.location.href = "https://www.instagram.com/";
+}
+
+// function soma(n1, n2){
+//     return n1, n2;
+// }
+
+// var validar = 0;
+
+// function validaIdade(idade){
+//     if(idade >= 18){
+//         validar = true
+//     }else{
+//         validar = false
+//     }
+// }
+
+// var idade = prompt("Qual sua idade?");
+// validaIdade(idade);
+// console.log(validar);
+
+// function setReplace(frase, nome, novo_nome){
+//     return frase.replace(nome, novo_nome)
+// }
+// alert(soma(4, 9));
+// alert(setReplace("Vai Japão", "EUA", "Brasil"));
+
+// var d = new Date();
+// alert(d.getMonth());
+
+
+// var count;
+// for(count = 0; count <= 5; count++){
+//     alert(count)
+// };
+
+// var count = 0;
+// while (count <= 5){
+//     console.log(count);
+//     count++;
+// };
+
+// var idade = prompt("Qual sua idade?");
+// if (idade >= 18){
+//     alert("Maior de Idade");
+// }else{
+//     alert("Menor de Idade");
+// }
+
+
+/*var frutas = [{nome:"maça", cor: "vermelha"}, {nome:"uva", cor: "roxa"}]//Dicionários
 console.log(frutas);
-alert(frutas[1].nome);
+alert(frutas[1].nome);*/
 
 
 /*var fruta = {nome:"maça", cor: "vermelha"}//Dicionários
